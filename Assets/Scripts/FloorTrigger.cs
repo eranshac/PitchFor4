bc94340ae256816a3cf621ca9de447fd8abfcb5a
eranshac <eranmatematika@gmail.com>
2015-09-04 02:01:17 +0300

    כל הפרוייקט

---
+++ Assets/Scripts/FloorTrigger.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloorTrigger : MonoBehaviour {
+
+	public static bool isTouchEnabledFirstTime=false;
+	public static bool isTouchEnabledallways=false;
+	private static GameObject pauseScreen;
+	void Start () {
+		pauseScreen=GameObject.Find("PauseScreen");
+		pauseScreen.SetActive (false);
+		print(pauseScreen.gameObject);
+
+
+
+	}
+	
+	// Update is called once per frame
+	public void LoadLevel (string name) {
+				print(pauseScreen.gameObject);
+
+		if(!isTouchEnabledFirstTime){
+			// load Pause Screen
+			pauseScreen.SetActive (true);
+			pauseScreen.gameObject.SetActive(true);
+			Time.timeScale = 0;
+
+			}
+			else{
+			//
+			if(isTouchEnabledallways){
+			Application.LoadLevel(name);
+			}
+		
+		}
+	}
+	void OnTriggerEnter2D(Collider2D flask){
+		print (flask.gameObject.tag);
+		if (flask.gameObject.tag =="MoreInformationFlask" && Application.loadedLevelName == "MainMenu") {
+			Application.LoadLevel("Options");
+		} 
+		if(flask.gameObject.tag =="PlayFlask"){
+			Application.LoadLevel ("GAME");
+		}
+		if(flask.gameObject.tag =="BackButton" && Application.loadedLevelName == "Options" ){
+			Application.LoadLevel ("MainMenu");
+		}
+		if(flask.gameObject.tag =="BackButton" && Application.loadedLevelName == "Settings" ){
+			Application.LoadLevel ("Options");
+		}
+		if(flask.gameObject.tag =="SettingFlask" && Application.loadedLevelName == "Options" ){
+			Application.LoadLevel ("Settings");
+		}
+
+	/*	if(flask.gameObject.tag ==""){
+			Application.LoadLevel ("");
+		}
+		if(flask.gameObject.tag ==""){
+			Application.LoadLevel ("");
+		}
+		if(flask.gameObject.tag ==""){
+			Application.LoadLevel ("");
+		}*/
+
+
+	}
+}
---
+++ Assets/Scripts/GetUserPitch.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GetUserPitch : MonoBehaviour {
+	private float time;
+	private Text HigestPitchText,LowestPitchText;
+	private float highestPitch=0,lowestpitch=1000;
+	private bool checkHigest = false,checkLowest=false;
+	public Slider highestPitchSlider;
+	public Slider lowestPitchSlider;
+	private float currentTimeForSlider;
+	// Use this for initialization
+	
+	
+	
+	public void CheckHighestPitch(){
+		highestPitchSlider.value=0;
+		currentTimeForSlider=Time.timeSinceLevelLoad;
+		checkHigest=true;
+		GameObject.Find("ButtonForHigh").GetComponent<Button>().interactable=false;
+		GameObject.Find("ButtonForLow").GetComponent<Button>().interactable=false;
+		Invoke("Stop",5);
+	}
+	public void CheckLowestPitch(){
+		lowestPitchSlider.value=0;
+		currentTimeForSlider=Time.timeSinceLevelLoad;
+		checkLowest=true;
+		GameObject.Find("ButtonForLow").GetComponent<Button>().interactable=false;
+		GameObject.Find("ButtonForHigh").GetComponent<Button>().interactable=false;
+		Invoke("Stop",5);
+	}
+	
+	public void Stop(){
+		GameObject.Find("ButtonForHigh").GetComponent<Button>().interactable=true;
+		GameObject.Find("ButtonForLow").GetComponent<Button>().interactable=true;
+	PlayerPrefsManager.SetHighestPitch(highestPitch);
+		PlayerPrefsManager.SetLowhestPitch(lowestpitch);
+	checkHigest=false;
+		checkLowest=false;
+	
+	}
+	
+	
+	void Start () {
+		HigestPitchText = (Text) GameObject.Find("HigestPitchText").GetComponent<Text>();
+		LowestPitchText = (Text) GameObject.Find("LowestPitchText").GetComponent<Text>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(checkHigest){
+			highestPitchSlider.value=(Time.timeSinceLevelLoad-currentTimeForSlider)/5;
+			
+		if(Controller.x>highestPitch)
+			highestPitch=Controller.x;
+			}
+			
+		if(checkLowest){
+			lowestPitchSlider.value=(Time.timeSinceLevelLoad-currentTimeForSlider)/5;
+			
+			if(Controller.x>0 && Controller.x<lowestpitch)
+				lowestpitch=Controller.x;
+		}
+			
+			
+			
+			
+		print("Controller.x " + Controller.x); 
+		HigestPitchText.text="Highest Pitch :" + highestPitch.ToString();
+		LowestPitchText.text="Lowest Pitch : " + lowestpitch.ToString();
+	
+	
+	}
+	
+
+}
